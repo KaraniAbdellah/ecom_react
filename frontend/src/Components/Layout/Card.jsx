ad0429c04ae9c5d5119974195de56e0c4f47b251
KaraniAbdellah <abdellahkarani@gmail.com>
2025-03-12 22:29:05 +0000

    change the login and registration

--- frontend/src/Components/Layout/Card.jsx
+++ frontend/src/Components/Layout/Card.jsx
@@ -5,8 +5,7 @@ export default function Card() {
   return (
     <div className="flex justify-center items-center bg-zinc-700 p-3 flex-col">
       <h1 className="text-lg font-semibold text-orange-500">
-        🛠️ This feature is currently under development. For any information,
-        contact us at abdellahlkarani@gmail.com
+        🛠️ This feature is currently under development. 🛠️
       </h1>
       <Link to="/">
         <button className="text-gray-600 mt-2 bg-gray-200 p-2 hover:bg-orange-500 hover:text-white rounded-full transition-all">
